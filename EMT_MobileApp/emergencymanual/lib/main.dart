b79d4c99fc87db570f4fbd6098dad4c8e6509f3e
u1083862 <u1083862@umail.utah.edu>
2021-04-01 10:45:06 -0600

    added the four buttons on the home page

--- EMT_MobileApp/emergencymanual/lib/main.dart
+++ EMT_MobileApp/emergencymanual/lib/main.dart
@@ -10,15 +10,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      title: 'EMT Manual',
+      home: Navigation(),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int dummy = 0;
+  void newLogPressed() {
+    setState(() {
+      dummy = dummy + 1;
+    });
+  }
+
+  void oldLogPressed() {
+    setState(() {
+      dummy = dummy + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Emergency Manual'),
       ),
+      body: new Center(
+          child: new Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+            new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new ElevatedButton(
+                    onPressed: newLogPressed,
+                    child: new Text(
+                      "Start New Log",
+                    ),
+                  ),
+                  new ElevatedButton(
+                    onPressed: oldLogPressed,
+                    child: new Text(
+                      "View Old Logs",
+                    ),
+                  ),
+                ]),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new ElevatedButton(
+                  onPressed: newLogPressed,
+                  child: new Text(
+                    "Protocols",
+                  ),
+                ),
+                new ElevatedButton(
+                  onPressed: newLogPressed,
+                  child: new Text(
+                    "Quick Links",
+                  ),
+                )
+              ],
+            ),
+          ])),
     );
   }
 }
