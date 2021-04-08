d8c8d4789ab6a5eac5bd71e15ed01523d2556dea
u1083862 <u1083862@umail.utah.edu>
2021-04-08 10:09:31 -0600

    Added old logs implementation

--- EMT_MobileApp/emergencymanual/lib/oldlogs.dart
+++ EMT_MobileApp/emergencymanual/lib/oldlogs.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
-class OldLogsPage extends StatelessWidget {
+class OldLogsPage extends StatefulWidget {
   @override
-  Widget build(BuildContext ctxt) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Old Logs"),
+  _OldLogsPageState createState() => _OldLogsPageState();
+}
+
+class _OldLogsPageState extends State<OldLogsPage> {
+  List logs = [];
+
+  @override
+  void initState() {
+    this._getLogs();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Logs'),
+        centerTitle: true,
+        backgroundColor: Color(0xFFFFFF),
       ),
-      body: new Text("Old Logs Page...!!!!!!"),
+      body: Container(
+        child: _buildLogsList(),
+      ),
+    );
+  }
+
+  void _getLogs() async {
+    //TODO: Where we will actually fetch from the database
+    //dummmy data right now
+    List tempList = ["Nov 10, 2020", "Nov 11, 2020", "Nov 12, 2020"];
+
+    setState(() {
+      logs = tempList;
+    });
+  }
+
+  Widget _buildLogsList() {
+    return ListView.builder(
+      itemCount: logs == null ? 0 : logs.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(logs[index]),
+          onTap: () => print(logs[index]),
+        );
+      },
     );
   }
 }
