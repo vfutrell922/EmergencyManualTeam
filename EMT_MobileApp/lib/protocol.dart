1c7c597e74aece01ae4b773c8f608a222ae59de2
u1083862 <u1083862@umail.utah.edu>
2021-04-29 10:54:07 -0600

    finished protocol page

--- EMT_MobileApp/lib/protocol.dart
+++ EMT_MobileApp/lib/protocol.dart
@@ -7,76 +7,114 @@ class ProtocolPage extends StatefulWidget {
 
 class _ProtocolState extends State<ProtocolPage> {
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cardiac Arrest'),
-        centerTitle: true,
-        backgroundColor: Color(0xFFFFFF),
-      ),
-      body: Scaffold(
-        appBar: _buildRoleBar(context),
-        body: Container(
-          child: _buildProtocolInfoList(),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProtocolInfoList() {
-    return new Center(child: new Column());
-  }
-
-  Widget _buildRoleBar(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double roleButtonWidth = (screenWidth - (screenWidth * .1)) / 5;
-    double roleButtonHeight = roleButtonWidth * 0.5;
-    return new Row(
-      children: <Widget>[
-        new SizedBox(
-          width: roleButtonWidth,
-          height: roleButtonHeight,
-          child: new ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.yellow,
-              onPrimary: Colors.black,
-            ),
-            onPressed: () => print("General Pressed"),
-            child: new Text("General"),
+    return DefaultTabController(
+        initialIndex: 1,
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Color(0xFFFFFF),
+              title: Text("Cardiac Arrest"),
+              bottom: new TabBar(
+                isScrollable: true,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.black,
+                tabs: [
+                  new Container(
+                    color: Colors.yellow,
+                    child: new Tab(
+                      child: Text("General"),
+                    ),
+                  ),
+                  new Container(
+                    color: Colors.blue,
+                    child: new Tab(
+                      child: Text("EMT"),
+                    ),
+                  ),
+                  new Container(
+                    color: Colors.green,
+                    child: new Tab(
+                      child: Text("AEMT"),
+                    ),
+                  ),
+                  new Container(
+                    color: Colors.red,
+                    child: new Tab(
+                      child: Text("Paramedic"),
+                    ),
+                  ),
+                  new Container(
+                    color: Colors.grey,
+                    child: new Tab(
+                      child: Text("Charts"),
+                    ),
+                  ),
+                ],
+              )),
+          body: TabBarView(
+            children: <Widget>[
+              Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.yellow,
+                  foregroundColor: Colors.black,
+                  title: Text(
+                    "General",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                body: Text('This is general info about cardiac arrest.'),
+              ),
+              Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.black,
+                  title: Text(
+                    "EMT",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                body: Text('This is EMT info about cardiac arrest.'),
+              ),
+              Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.black,
+                  title: Text(
+                    "AEMT",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                body: Text('This is AEMT info about cardiac arrest.'),
+              ),
+              Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.black,
+                  title: Text(
+                    "Paramedic",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                body: Text('This is Paramedic info about cardiac arrest.'),
+              ),
+              Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  backgroundColor: Colors.grey,
+                  foregroundColor: Colors.black,
+                  title: Text(
+                    "Charts",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                body: Text('These are relevant charts for cardiac arrest'),
+              ),
+            ],
           ),
-        ),
-        new SizedBox(
-          width: roleButtonWidth,
-          height: roleButtonHeight,
-          child: new ElevatedButton(
-            onPressed: () => print("EMT Pressed"),
-            child: new Text("EMT"),
-          ),
-        ),
-        new SizedBox(
-          width: roleButtonWidth,
-          height: roleButtonHeight,
-          child: new ElevatedButton(
-            onPressed: () => print("AEMT Pressed"),
-            child: new Text("AEMT"),
-          ),
-        ),
-        new SizedBox(
-          width: roleButtonWidth,
-          height: roleButtonHeight,
-          child: new ElevatedButton(
-            onPressed: () => print("Paramedic Pressed"),
-            child: new Text("Paramedic"),
-          ),
-        ),
-        new SizedBox(
-          width: roleButtonWidth,
-          height: roleButtonHeight,
-          child: new ElevatedButton(
-            onPressed: () => print("Charts Pressed"),
-            child: new Text("Charts"),
-          ),
-        ),
-      ],
-    );
+        ));
   }
 }
