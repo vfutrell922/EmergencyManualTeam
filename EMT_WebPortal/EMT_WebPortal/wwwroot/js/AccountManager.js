2ce049681dc0b946069a0d83745e35032e47b08a
Vincent Futrell <vincent.futrell@gmail.com>
2021-09-23 09:09:06 -0600

    Added the scripts to make the dropdown title reflect the current selection.

---
+++ EMT_WebPortal/EMT_WebPortal/wwwroot/js/AccountManager.js
@@ -0,0 +1,18 @@
+﻿function selectCaregiver() {
+    var user = event.target.getAttribute('data-arg1');
+    const dropdownButton = document.getElementById("dropdownMenuButton " + user);
+    dropdownButton.textContent = 'Caregiver';
+}
+
+function selectAdministrator() {
+    var user = event.target.getAttribute('data-arg1');
+    const dropdownButton = document.getElementById("dropdownMenuButton " + user);
+    dropdownButton.textContent = 'Administrator';
+}
+
+function selectWebMaster() {
+    var user = event.target.getAttribute('data-arg1');
+    const dropdownButton = document.getElementById("dropdownMenuButton " + user);
+    dropdownButton.textContent = 'WebMaster';
+}
+
