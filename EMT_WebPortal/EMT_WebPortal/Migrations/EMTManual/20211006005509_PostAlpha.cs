9fa6691c92dfa8e408cc86761774b932be4712ed
Vincent Futrell <vincent.futrell@gmail.com>
2021-10-05 19:02:02 -0600

    Added logo to the root files

---
+++ EMT_WebPortal/EMT_WebPortal/Migrations/EMTManual/20211006005509_PostAlpha.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EMT_WebPortal.Migrations.EMTManual
+{
+    public partial class PostAlpha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chart",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    IsQuickLink = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chart", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guideline",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Background = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Checklist = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guideline", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneNumber",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    hospitalName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    numberString = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneNumber", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Admin = table.Column<bool>(type: "bit", nullable: false),
+                    Certification = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Protocol",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Certification = table.Column<int>(type: "int", nullable: false),
+                    PatientType = table.Column<int>(type: "int", nullable: false),
+                    HasAssociatedMedication = table.Column<bool>(type: "bit", nullable: false),
+                    ChartID = table.Column<int>(type: "int", nullable: true),
+                    OtherInformation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TreatmentPlan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GuidelineId = table.Column<int>(type: "int", nullable: false),
+                    OLMCRequired = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Protocol", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Protocol_Chart_ChartID",
+                        column: x => x.ChartID,
+                        principalTable: "Chart",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Protocol_Guideline_GuidelineId",
+                        column: x => x.GuidelineId,
+                        principalTable: "Guideline",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medication",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Indication = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contraindication = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precaution = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdverseEffects = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdultDosage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ChildDosage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProtocolID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medication", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Medication_Protocol_ProtocolID",
+                        column: x => x.ProtocolID,
+                        principalTable: "Protocol",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medication_ProtocolID",
+                table: "Medication",
+                column: "ProtocolID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Protocol_ChartID",
+                table: "Protocol",
+                column: "ChartID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Protocol_GuidelineId",
+                table: "Protocol",
+                column: "GuidelineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medication");
+
+            migrationBuilder.DropTable(
+                name: "PhoneNumber");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Protocol");
+
+            migrationBuilder.DropTable(
+                name: "Chart");
+
+            migrationBuilder.DropTable(
+                name: "Guideline");
+        }
+    }
+}
