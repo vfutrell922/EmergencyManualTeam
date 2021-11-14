4faf672f09250b188844693ebad97e0260e6ff9d
Vincent Futrell <vincent.futrell@gmail.com>
2021-11-14 16:09:12 -0700

    Added documentation to the javascript files

--- EMT_WebPortal/EMT_WebPortal/wwwroot/js/Delete.js
+++ EMT_WebPortal/EMT_WebPortal/wwwroot/js/Delete.js
@@ -1,4 +1,9 @@
-﻿$(document).ready(function () {
+﻿/*
+ * Author: Vincent Futrell
+ * Date Last Modified: 11/13/2021
+ * This file contains the script to control the delete modal
+ */
+$(document).ready(function () {
     $('#confirmDelete').on('show.bs.modal', function (e) {
         $('#deleteConfirmButton').attr('href', $(e.relatedTarget).data('href'));
     });
