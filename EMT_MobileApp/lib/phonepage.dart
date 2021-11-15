0683f5610e3fd6d153231b5985790240bd28c90a
u1083862 <u1083862@umail.utah.edu>
2021-11-14 18:05:18 -0700

    Another leftover function

--- EMT_MobileApp/lib/phonepage.dart
+++ EMT_MobileApp/lib/phonepage.dart
@@ -59,15 +59,6 @@ class _PhonePageState extends State<PhonePage> {
     });
   }
 
-  Widget PhoneNumberEntry(PhoneNumber pn) {
-    return new ListTile(
-      title: Text(pn.hospitalName),
-      onTap: () {
-        launch("tel://" + pn.numberString);
-      },
-    );
-  }
-
   Widget _buildPhoneNumsList() {
     return ListView.builder(
       itemCount: phonenums.length,
