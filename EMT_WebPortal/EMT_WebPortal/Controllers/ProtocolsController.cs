09724af6093d12043a7b8bf84381fc2f780095a5
Vincent Futrell <vincent.futrell@gmail.com>
2021-11-16 12:27:09 -0700

    Fixed bugs with the medication select

--- EMT_WebPortal/EMT_WebPortal/Controllers/ProtocolsController.cs
+++ EMT_WebPortal/EMT_WebPortal/Controllers/ProtocolsController.cs
@@ -81,19 +81,21 @@ namespace EMT_WebPortal.Controllers
                 _context.Add(protocol);
                 await _context.SaveChangesAsync();
 
-               // int task = await _context.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT EMTManualContext.dbo.Medications_Protocols ON;");
-                foreach(int i in MedicationsIdList) 
+                if (protocol.HasAssociatedMedication)
                 {
-                    MedicationProtocol mp = new MedicationProtocol()
+                    foreach (int i in MedicationsIdList)
                     {
-                        MedicationId = i,
-                        ProtocolId = protocol.Id,
-                        Protocol = protocol,
-                        Medication = _context.Medications.Where(x => x.ID == i).FirstOrDefault()
-                    };
-                    _context.Medications_Protocols.Add(mp);
+                        MedicationProtocol mp = new MedicationProtocol()
+                        {
+                            MedicationId = i,
+                            ProtocolId = protocol.Id,
+                            Protocol = protocol,
+                            Medication = _context.Medications.Where(x => x.ID == i).FirstOrDefault()
+                        };
+                        _context.Medications_Protocols.Add(mp);
+                    }
+                    _context.SaveChanges();
                 }
-                _context.SaveChanges();
                // await _context.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT dbo.Medications_Protocols OFF;");
                 return RedirectToAction(nameof(Index));
             }
